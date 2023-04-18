59026bb3000d522eadeb38b7ba85850fa1c3dcf0
Robert Kolozsi <robert.kolozsi@gmail.com>
2023-04-18 11:49:34 +0200

    New settings, updates and flake8 set no warning if line longer than 80 chars

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -100,8 +100,8 @@ cmp.setup {
       vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
       -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
       vim_item.menu = ({
-        luasnip = "[LSP]",
-        luasnip = "[Nvim_Lua]",
+        nvim_lsp = "[LSP]",
+        nvim_lua = "[Nvim_Lua]",
         luasnip = "[Snippet]",
         buffer = "[Buffer]",
         path = "[Path]",
