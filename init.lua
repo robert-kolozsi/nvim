883b37affa974f39bb2e62cc9d54b8da1a6a2605
Robert Kolozsi <robert.kolozsi@gmail.com>
2026-01-27 16:00:02 +0100

    Code clean up

--- init.lua
+++ init.lua
@@ -1,17 +1,4 @@
 -- Plugins
-require "config.keymaps"
+require("config.keymaps")
 require("config.lazy")
-require "config.options"
---require "user.plugins"
---require "user.colorscheme"
---require "user.cmp"
---require "user.lsp"
---require "user.telescope"
---require "user.treesitter"
---require "user.autopairs"
---require "user.comment"
---require "user.gitsigns"
---require "user.nvim-tree"
---require "user.bufferline"
---require "user.marks"
---require "user.lualine"
+require("config.options")
